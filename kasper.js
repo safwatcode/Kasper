5c8103f23d97d54714a5cf64c981251af66020df
Safwat Ahmed <safwatahmed669@gmail.com>
2025-07-23 21:12:15 +0300

    Finished Header and Landing design

---
+++ kasper.js
@@ -0,0 +1,32 @@
+// Slider
+
+
+// Initialize first slide
+showSlide(0);
+
+// Optional: Auto-slide every 5 seconds
+// setInterval(() => rightArrow.click(), 5000);
+
+// Automatically update current year in footer
+let year = new Date();
+let currentYear = year.getFullYear();
+document.getElementById("current-year").innerHTML = currentYear;
+
+// Count-Up Animation
+const counters = document.querySelectorAll(".stat-number");
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const current = +counter.innerText;
+    const increment = target / 200;
+    if (current < target) {
+      counter.innerText = `${Math.ceil(current + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
