9bc9ebcc6ffe2984b2b570d477d6ec87ef6889fa
Safwat Ahmed <safwatahmed669@gmail.com>
2025-07-26 11:48:51 +0300

    Finished Statistics section

--- kasper.js
+++ kasper.js
@@ -6,20 +6,20 @@ let currentYear = year.getFullYear();
 document.getElementById("current-year").innerHTML = currentYear;
 
 // Count-Up Animation
-const counters = document.querySelectorAll(".stat-number");
-counters.forEach((counter) => {
-  counter.innerText = "0";
+// const counters = document.querySelectorAll(".stat-number");
+// counters.forEach((counter) => {
+//   counter.innerText = "0";
 
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const current = +counter.innerText;
-    const increment = target / 200;
-    if (current < target) {
-      counter.innerText = `${Math.ceil(current + increment)}`;
-      setTimeout(updateCounter, 10);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  updateCounter();
-});
+//   const updateCounter = () => {
+//     const target = +counter.getAttribute("data-target");
+//     const current = +counter.innerText;
+//     const increment = target / 200;
+//     if (current < target) {
+//       counter.innerText = `${Math.ceil(current + increment)}`;
+//       setTimeout(updateCounter, 10);
+//     } else {
+//       counter.innerText = target;
+//     }
+//   };
+//   updateCounter();
+// });
